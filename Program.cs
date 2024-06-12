4da3105067e4cd23d517efe8b7a6b180c34abf0e
IgorProPrograming <igorgolanski@hotmail.com>
2024-06-12 15:57:05 +0200

    marge check methode toegevoegd

--- Program.cs
+++ Program.cs
@@ -1,36 +1,18 @@
 ﻿using Container_algoritme.models;
-using System.Diagnostics;
 
-Main M = new Main();
+Ship ship;
 
-M.SetShipDimensions();
-M.Sort();
-M.GetResults();
+Console.WriteLine("------------------------------------");
+Console.WriteLine("ship x:");
+int shipX = int.Parse(Console.ReadLine());
+Console.WriteLine("ship y:");
+int shipY = int.Parse(Console.ReadLine());
+Console.WriteLine("------------------------------------");
 
-public class Main()
-{
-    Ship ship;
-    public void SetShipDimensions()
-    {
-        Console.WriteLine("------------------------------------");
-        Console.WriteLine("ship x:");
-        string shipX = Console.ReadLine();
-        Console.WriteLine("ship y:");
-        string shipY = Console.ReadLine();
-        Console.WriteLine("------------------------------------");
+ship = new Ship(shipX, shipY);
 
-        ship = new Ship( Convert.ToInt32(shipX), Convert.ToInt32(shipY));
-    }
+ship.FillShip();
 
-    public void Sort()
-    {
-        ship.FillShip();
-    }
-
-    public void GetResults()
-    {
-        string webString = ship.WebStringMaker();
-        //System.Diagnostics.Process.Start("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", webString);
-        Console.WriteLine(webString);
-    }
-}
+string webString = ship.WebStringMaker();
+System.Diagnostics.Process.Start("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", webString);
+Console.WriteLine(webString);
--- models/Ship.cs
+++ models/Ship.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Container_algoritme.models
 {
     internal class Ship
     {
-        List<Container> containerList = new List<Container>();
-        List<Container> shipSlot = new List<Container>();
-        List<Container>[,] containerLayout;
+        private List<Container> containerList = new List<Container>();
+        private List<Container>[,] containerLayout;
 
-        int length;
-        int width;
+        private int length;
+        private int width;
 
         public Ship(int x, int y)
         {
@@ -25,12 +26,7 @@ namespace Container_algoritme.models
             InitializeShipGrid();
         }
 
-        public void AddContainer(Container container, int x, int y)
-        {
-            containerLayout[x, y].Add(container);
-        }
-
-        public void InitializeShipGrid()
+        private void InitializeShipGrid()
         {
             for (int i = 0; i < containerLayout.GetLength(0); i++)
             {
@@ -42,12 +38,7 @@ namespace Container_algoritme.models
 
         }
 
-        public void AddContainer(Container container)
-        {
-            containerList.Add(container);
-        }
-
-        public void FillShipList()
+        private void FillShipList()
         {
             containerList.AddRange(new List<Container> {
             new Container { id = 1, isValuable = false, isCooled = false, weight = 30 },
@@ -63,11 +54,36 @@ namespace Container_algoritme.models
             new Container { id = 11, isValuable = true, isCooled = false, weight = 15 },
             new Container { id = 12, isValuable = true, isCooled = false, weight = 20 }
             });
-        }
 
-        public Container GetContainer(int id)
-        {
-            return containerList.Find(x => x.id == id);
+            containerList.AddRange(new List<Container> {
+            new Container { id = 1, isValuable = false, isCooled = false, weight = 30 },
+            new Container { id = 2, isValuable = false, isCooled = false, weight = 25 },
+            new Container { id = 3, isValuable = false, isCooled = false, weight = 10 },
+            new Container { id = 4, isValuable = false, isCooled = false, weight = 5 },
+            new Container { id = 5, isValuable = false, isCooled = false, weight = 10 },
+            new Container { id = 6, isValuable = false, isCooled = true, weight = 15 },
+            new Container { id = 7, isValuable = false, isCooled = true, weight = 20 },
+            new Container { id = 8, isValuable = false, isCooled = true, weight = 25 },
+            new Container { id = 9, isValuable = false, isCooled = true, weight = 30 },
+            new Container { id = 10, isValuable = true, isCooled = false, weight = 10 },
+            new Container { id = 11, isValuable = true, isCooled = false, weight = 15 },
+            new Container { id = 12, isValuable = true, isCooled = false, weight = 20 }
+            });
+
+            containerList.AddRange(new List<Container> {
+            new Container { id = 1, isValuable = false, isCooled = false, weight = 30 },
+            new Container { id = 2, isValuable = false, isCooled = false, weight = 25 },
+            new Container { id = 3, isValuable = false, isCooled = false, weight = 10 },
+            new Container { id = 4, isValuable = false, isCooled = false, weight = 5 },
+            new Container { id = 5, isValuable = false, isCooled = false, weight = 10 },
+            new Container { id = 6, isValuable = false, isCooled = true, weight = 15 },
+            new Container { id = 7, isValuable = false, isCooled = true, weight = 20 },
+            new Container { id = 8, isValuable = false, isCooled = true, weight = 25 },
+            new Container { id = 9, isValuable = false, isCooled = true, weight = 30 },
+            new Container { id = 10, isValuable = true, isCooled = false, weight = 10 },
+            new Container { id = 11, isValuable = true, isCooled = false, weight = 15 },
+            new Container { id = 12, isValuable = true, isCooled = false, weight = 20 }
+            });
         }
 
         public string WebStringMaker()
@@ -147,7 +163,23 @@ namespace Container_algoritme.models
             return webString;
         }
 
-        private bool CanPutContainerOnTop(int x, int y, Container container = null)
+        private bool IsContainerAccesible(int x, int y)
+        {
+            //check of voor of achter container ruimte is
+            int z = containerLayout[x, y].Count;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                int newY = y + i;
+                if (newY >= 0 && newY < containerLayout.GetLength(1) && containerLayout[x, newY].Count <= z)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool CanPutContainerOnTop(int x, int y, Container container)
         {
             //check of er uberhaupt iets staat
             if (!containerLayout[x, y].Any())
@@ -181,24 +213,93 @@ namespace Container_algoritme.models
             return true;
         }
 
+      private bool isMarginCorrect()
+        {
+            //check of de marge van 20% niet overschreden wordt
+            float leftWeight = 0;
+            float rightWeight = 0;
+            int middleRowWeight = 0;
+            float halfOfMiddleRowWeight = 0;
+
+            int leftSide;
+            int rightSide;
+
+            if (!(width % 2 == 0))
+            {
+
+                int middleRow = (int)Math.Floor(width / 2.0 + 0.5);
+                leftSide = middleRow - 1;
+                rightSide = middleRow + 1;
+
+                for (int x = 0; x < width; x++)
+                {
+                    foreach(var container in containerLayout[x,middleRow])
+                    {
+                        middleRowWeight += container.weight;
+                    }
+                }
+
+                halfOfMiddleRowWeight = middleRowWeight / 2;
+            } else
+            {
+                leftSide = width / 2;
+                rightSide = width - 1;
+            }
+
+            for (int x = 0; x <= leftSide; x++)
+            {
+                for (int y = 0; y <= length - 1; y++) {
+                    foreach (var container in containerLayout[x,y])
+                    {
+                        leftWeight += container.weight;
+                    }
+                }
+                        
+            }
+
+            for (int x = rightSide; x <= width - 1; x++)
+            {
+                for (int y = 0; y <= length - 1; y++)
+                {
+                    foreach (var container in containerLayout[x, y])
+                    {
+                        rightWeight += container.weight;
+                    }
+                }
+            }
+
+            leftWeight += halfOfMiddleRowWeight;
+            rightWeight += halfOfMiddleRowWeight;
+
+            if (Math.Abs(leftWeight - rightWeight) > (leftWeight + rightWeight) * 0.2)
+            {
+                return false;
+            } 
+            else 
+            { 
+                return true; 
+            }
+        }
+
         public void FillShip()
         {
             while (containerList.Any())
             {
-
                 // eerst gekoelde containers
-
-                for (int x = 0; x < containerLayout.GetLength(0); x++)
+                while (containerList.Any(container => container.isCooled))
                 {
-                    foreach (var container in containerList)
+                    for (int x = 0; x < containerLayout.GetLength(0); x++)
                     {
-                        if (CanPutContainerOnTop(x, 0, container))
+                        foreach (var container in containerList)
                         {
-                            if (container.isCooled)
+                            if (CanPutContainerOnTop(x, 0, container))
                             {
-                                containerLayout[x, 0].Add(container);
-                                containerList.Remove(container);
-                                break;
+                                if (container.isCooled)
+                                {
+                                    containerLayout[x, 0].Add(container);
+                                    containerList.Remove(container);
+                                    break;
+                                }
                             }
                         }
                     }
@@ -223,10 +324,7 @@ namespace Container_algoritme.models
                     }
                 }
 
-
-
                 // waardevolle containers
-
 
                 for (int y = 0; y < containerLayout.GetLength(1); y++)
                 {
@@ -236,7 +334,7 @@ namespace Container_algoritme.models
                         {
                             if (container.isValuable)
                             {
-                                if (CanPutContainerOnTop(x, y, container))
+                                if (CanPutContainerOnTop(x, y, container) & IsContainerAccesible(x,y))
                                     containerLayout[x, y].Add(container);
                                 containerList.Remove(container);
                                 break;
@@ -244,6 +342,11 @@ namespace Container_algoritme.models
                         }
                     }
                 }
+            }
+
+            if (!isMarginCorrect())
+            {
+
             }
         }
     }
